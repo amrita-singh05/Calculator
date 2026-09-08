40637659a0275a7351e955295ba3e51245ecd690
Amrita Singh <amritasingh8283@gmail.com>
2026-09-08 13:31:15 +0530

    calculator

---
+++ New Alpha Tasks/task 1/script.js
@@ -0,0 +1,3 @@
+const images = document.querySelectorAll("img")
+const nextButton = document.querySelector("Next")
+const previousButton = document.querySelector("Previous")
---
+++ Web Development Course/01_First Web Page/script.js
@@ -0,0 +1,1 @@
+alert("Amrita ki first website")
